e16815b52a821404810e24adfa5d5cd996a17216
Gal <Gal@DESKTOP-QG7KMQ2>
2020-03-14 23:29:47 +0200

    change name

--- Servlet/src/a/Example.java
+++ Servlet/src/a/Example.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-public class Example {
+public class DataBase {
 	  public static void main(String[] args) {
         Connection connection=null;
         try {
